6b87e08e3cf47cfea2c315da5fd1072a4fdbb1f8
wildersitow <wpverdecia@unicesar.edu.co>
2026-07-24 18:02:57 -0500

    merge: Merge con la rama fix/import-enum-contratos a main
    * fix: Bug en imports de Enum clase Contratos.py

--- Modulos/Contratos.py
+++ Modulos/Contratos.py
@@ -1,5 +1,5 @@
 from Modulos.enums import TipoMantenimiento
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Enum as SQLEnum
 from datetime import datetime
 from base_de_datos import Base, SessionLocal, engine
 
