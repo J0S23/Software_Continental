d7bdead7d5667c62bd7967abb3a7e955e105a97d
wildersitow <wpverdecia@unicesar.edu.co>
2026-07-23 21:46:29 -0500

    Merge: Merge de feature/fusionar-servicios a main
    * chore: eliminar carpeta duplicada Software_Continental
    
    * feat: Atributo seriedad creado
    
    * feat: Cambio de nombre en informes a informes_mensuales
    
    * feat: Clase TipoEquipo movida de modelos.py a enums.py
    
    * feat: Clase EstadoEquipo movida de modelos.py a enums.py
    
    * feat: Carpeta variables cambiada de nombre a modulos

--- modelos.py
+++ modelos.py
@@ -2,21 +2,3 @@
 import enum
 from sqlalchemy import Column, Integer, String, Boolean, Enum as SQLEnum
 from database import Base
-
-
-class TipoEquipo(str, enum.Enum):
-    MULTIFUNCIONAL = "multifuncional"
-    IMPRESORA = "impresora"
-    ESCANER = "escaner"
-    PLOTTER = "plotter"
-    OTRO = "otro"
-
-
-class EstadoEquipo(str, enum.Enum):
-    DISPONIBLE = "disponible"
-    INSTALADO = "instalado"
-    EN_MANTENIMIENTO = "en_mantenimiento"
-    EN_REPARACION = "en_reparacion"
-    DE_RESPALDO = "de_respaldo"
-    RETIRADO = "retirado"
-    DADO_DE_BAJA = "dado_de_baja"
