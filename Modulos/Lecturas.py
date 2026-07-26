c2a456e6bed58313289f42c94ee5fbce019c50ad
wpverdecia <wpverdecia@unicesar.edu.co>
2026-07-26 15:21:12 -0500

    feat: Agregar atributos en Lecturas.py

--- Modulos/Lecturas.py
+++ Modulos/Lecturas.py
@@ -8,9 +8,15 @@ class Lecturas(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     equipo_id = Column(Integer)
+    #Atributos que permiten saber las lecturas de un contrato en un periodo
+    contrato_id = Column(Integer, nullable=True)
+    cliente_id = Column(Integer, nullable=True)
+    periodo = Column(String, nullable=True)
     medio_lectura = Column(String)
     estado_lectura = Column(String)
-    contador = Column(Integer, default=0)
+    #Dos contadores para separar entre blanco y negro y color
+    contador_bn = Column(Integer, default=0)
+    contador_color = Column(Integer, default=0)
     fecha_lectura = Column(DateTime, default=datetime.utcnow)
     fecha_creacion = Column(DateTime, default=datetime.utcnow)
     
@@ -19,14 +25,21 @@ class Lecturas(Base):
         Base.metadata.create_all(bind=engine)
     
     @staticmethod
-    def agregar(equipo_id, medio_lectura, estado_lectura, contador=0, fecha_lectura=None):
+    def agregar(equipo_id, medio_lectura, estado_lectura,
+        contador_bn=0, contador_color=0,
+        contrato_id=None, cliente_id=None, periodo=None,
+        fecha_lectura=None,):
         """Agrega una lectura a la BD"""
         db = SessionLocal()
         nueva_lectura = Lecturas(
             equipo_id=equipo_id,
             medio_lectura=medio_lectura,
             estado_lectura=estado_lectura,
-            contador=contador,
+            contador_bn=contador_bn,
+            contador_color=contador_color,
+            contrato_id=contrato_id,
+            cliente_id=cliente_id,
+            periodo=periodo,
             fecha_lectura=fecha_lectura or datetime.utcnow()
         )
         db.add(nueva_lectura)
